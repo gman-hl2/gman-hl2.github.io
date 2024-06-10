8733d9f5c9e324d0279e7804dd74d010a2cc3411
gman-hl2 <gmanhl2gmod38@gmail.com>
2024-06-10 11:13:46 +0400

    Update scrips.js

--- scrips.js
+++ scrips.js
@@ -17,4 +17,18 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         });
     }
+
+    // Back to top button
+    const backToTopButton = document.getElementById('back-to-top');
+    window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 300) {
+            backToTopButton.style.display = 'block';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+
+    backToTopButton.addEventListener('click', function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
 });
